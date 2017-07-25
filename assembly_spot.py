8173254d59a56c5870e060c66c8798224dc69368
etsakov <eitsakov@yahoo.com>
2017-07-25 18:00:46 +0300

    Working trading app but not timer and smart prediction for now

--- assembly_spot.py
+++ assembly_spot.py
@@ -97,7 +97,7 @@ def trades_pip_margin_indicator(trades_stream_data, structured_price_data):
     a = 0
     while a != 1:
         time_now = datetime.utcnow()
-        print(time_now)
+        print("Current time: ", time_now)
 
         for cur_trades in quasi_stream_trades_info_generator(ACCESS_TOKEN, ACCOUNT_ID):
             full_trades_data = cur_trades
@@ -142,103 +142,100 @@ def shows_trade_units_available(ACCESS_TOKEN, ACCOUNT_ID):
 
 def rate_direction_predictor(trades_stream_data, trade_units_available, structured_price_data, INSTRUMENTS):
     # Chooses direction for the first deal
-    d = 0
-    while d != 1:
-        print('trade_units_available: ', trade_units_available)
-        for tsd_item in trades_stream_data:
-            print(len(tsd_item))
-            if len(tsd_item) > 0:
-                pass
-            else:
-                for struct_price_data_item in structured_price_data:
-                    if len(struct_price_data_item[1]) < 5:
-                        pass
-                    else:
-                        last_price = float(struct_price_data_item[1][-1])
-                        last_five_item = 0
-                        last_five_items_sum = 0
-                        for f in struct_price_data_item[1][-5:]:
-                            last_five_items_sum += float(struct_price_data_item[1][-5:][last_five_item])
-                            last_five_item += 1
-                        last_five_avg = float(format(float(last_five_items_sum) / 5, '.5f'))
-                        print('Last price', last_price)
-                        print('Last five average', last_five_avg)
-                        if last_price < last_five_avg:
-                            direction = '-'
-                            take_profit_price = format(last_price - 0.0003, '.5f')
-                        else:
-                            direction = ''
-                            take_profit_price = format(last_price + 0.0003, '.5f')
-
-                        amount_of_units = int(int(trade_units_available) / 10)
-                        units_quantity = direction + str(amount_of_units)
-                        data = {
-                                "order": {
-                                "timeInForce": "FOK",
-                                "instrument": INSTRUMENTS,
-                                "units": units_quantity,  
-                                "type": "MARKET",
-                                "positionFill": "DEFAULT",
-                                "takeProfitOnFill": {
-                                    "timeInForce": "GTC",
-                                    "price": take_profit_price
-                                }
-                            }
-                        }
-
-                        client = oandapyV20.API(access_token = ACCESS_TOKEN)
-                        r = orders.OrderCreate(accountID = ACCOUNT_ID, data = data)
-                        client.request(r)
-                        print(r.response)
-                        
+    # d = 0
+    # while d != 1:
+    print('trade_units_available: ', trade_units_available)
+    for tsd_item in trades_stream_data:
+        if len(tsd_item) > 0:
+            print('We have currently ', len(tsd_item), ' active transactions.')
             break
-            # TO DO - It semms to be correct to initiate two parallel programms: to collect the data and to initiate trades.
+        else:
+            for struct_price_data_item in structured_price_data:
+                if len(struct_price_data_item[1]) < 5:
+                    print("Not enough data to choose the direction :((")
+                    continue
+                else:
+                    last_price = float(struct_price_data_item[1][-1])
+                    last_item = 0
+                    last_five_items_sum = 0
+                    for f in struct_price_data_item[1][-5:]:
+                        last_five_items_sum += float(struct_price_data_item[1][-5:][last_item])
+                        last_item += 1
+                    last_five_avg = float(format(float(last_five_items_sum) / 5, '.5f'))
+                    print('Last price', last_price)
+                    print('Last five average', last_five_avg)
+                    if last_price < last_five_avg:
+                        direction = '-'
+                        take_profit_price = format(last_price - 0.0003, '.5f')
+                        break
+                    else:
+                        direction = ''
+                        take_profit_price = format(last_price + 0.0003, '.5f')
+                        break
+
+            amount_of_units = int(int(trade_units_available) / 10)
+            units_quantity = direction + str(amount_of_units)
+            data = {
+                    "order": {
+                    "timeInForce": "FOK",
+                    "instrument": INSTRUMENTS,
+                    "units": units_quantity,  
+                    "type": "MARKET",
+                    "positionFill": "DEFAULT",
+                    "takeProfitOnFill": {
+                        "timeInForce": "GTC",
+                        "price": take_profit_price
+                    }
+                }
+            }
+
+            client = oandapyV20.API(access_token = ACCESS_TOKEN)
+            r = orders.OrderCreate(accountID = ACCOUNT_ID, data = data)
+            client.request(r)
+            print(r.response)
+            break
+        break
+                    
+        
+        # TO DO - It semms to be correct to initiate two parallel programms: to collect the data and to initiate trades.
 
 
 def following_trades_creator(trades_stream_data, compare_heartbeat, trade_units_available, structured_price_data, INSTRUMENTS):
     # Once the initial trade is open makes further trades
-    c = 0
-    while c != 1:
-        for tsd_item in trades_stream_data:
-            print(len(tsd_item))
-            if len(tsd_item) == 0:
-                pass
+    # c = 0
+    # while c != 1:
+    for tsd_item in trades_stream_data:
+        print(len(tsd_item))
+        if len(tsd_item) == 0:
+            break
+        else:
+            for trade in compare_heartbeat:
+                unit = len(trade)
+                trade_amount = trade[0]['trade_amount']
+                trade_profit = trade[0]['trade_' + str(unit)]
+                print('trade_amount ', trade_amount, ' ', type(trade_amount))
+                print('trade_profit ', trade_profit, ' ', type(trade_profit))
+                break
+
+            for price_lists in structured_price_data:
+                ask_rate = price_lists[1][-1]
+                bid_rate = price_lists[2][-1]
+                print('ask_rate ', ask_rate, ' ', type(ask_rate))
+                print('bid_rate ', bid_rate, ' ', type(bid_rate))
+                break
+
+            print('Trade units left: ', trade_units_available)
+
+            if int(trade_units_available) < abs(trade_amount):
+                print('!!!Not enough units to trade!!!')
+                break
+
             else:
-                for trade in compare_heartbeat:
-                    unit = len(trade)
-                    trade_amount = trade[0]['trade_amount']
-                    trade_profit = trade[0]['trade_' + str(unit)]
-                    print('trade_amount ', trade_amount, ' ', type(trade_amount))
-                    print('trade_profit ', trade_profit, ' ', type(trade_profit))
-                    break
-
-                for price_lists in structured_price_data:
-                    ask_rate = price_lists[1][-1]
-                    bid_rate = price_lists[2][-1]
-                    print('ask_rate ', ask_rate, ' ', type(ask_rate))
-                    print('bid_rate ', bid_rate, ' ', type(bid_rate))
-                    break
-
-                print('Trade units left: ', trade_units_available)
-
-                if int(trade_units_available) < abs(trade_amount):
-                    print('!!!Not enough units to trade!!!')
-
-                else:
-                    if trade_profit < -5:
-                        if trade_amount > 0:
-                            take_profit_price = format(ask_rate + 0.0002, '.5f')
-                        else:
-                            take_profit_price = format(bid_rate - 0.0002, '.5f')
-                    elif trade_profit > 1:
-                        if trade_amount > 0:
-                            take_profit_price = format(ask_rate + 0.0001, '.5f')
-                        else:
-                            take_profit_price = format(bid_rate - 0.0001, '.5f')
-
+                if trade_profit < -3:
+                    if trade_amount > 0:
+                        take_profit_price = format(ask_rate + 0.0002, '.5f')
                     else:
-                        pass
-
+                        take_profit_price = format(bid_rate - 0.0002, '.5f')
                     data = {
                             "order": {
                             "timeInForce": "FOK",
@@ -252,13 +249,39 @@ def following_trades_creator(trades_stream_data, compare_heartbeat, trade_units_
                             }
                         }
                     }
-
                     client = oandapyV20.API(access_token = ACCESS_TOKEN)
                     r = orders.OrderCreate(accountID = ACCOUNT_ID, data = data)
                     client.request(r)
                     print(r.response)
-            break
-        time.sleep(1)
+                    break
+                elif trade_profit > 1:
+                    if trade_amount > 0:
+                        take_profit_price = format(ask_rate + 0.0001, '.5f')
+                    else:
+                        take_profit_price = format(bid_rate - 0.0001, '.5f')
+                    data = {
+                            "order": {
+                            "timeInForce": "FOK",
+                            "instrument": INSTRUMENTS,
+                            "units": str(trade_amount),  
+                            "type": "MARKET",
+                            "positionFill": "DEFAULT",
+                            "takeProfitOnFill": {
+                                "timeInForce": "GTC",
+                                "price": take_profit_price
+                            }
+                        }
+                    }
+                    client = oandapyV20.API(access_token = ACCESS_TOKEN)
+                    r = orders.OrderCreate(accountID = ACCOUNT_ID, data = data)
+                    client.request(r)
+                    print(r.response)
+                    break
+
+                else:
+                    break
+        # break
+        # time.sleep(1)
 
 
 if __name__=="__main__":
@@ -267,7 +290,69 @@ if __name__=="__main__":
     structured_price_data = read_stream_data_generator(stream_generator)
     compare_heartbeat = trades_pip_margin_indicator(trades_stream_data, structured_price_data)
     trade_units_available = shows_trade_units_available(ACCESS_TOKEN, ACCOUNT_ID)
-    rate_direction_predictor(trades_stream_data, trade_units_available, structured_price_data, INSTRUMENTS)
-    following_trades_creator(trades_stream_data, compare_heartbeat, trade_units_available, structured_price_data, INSTRUMENTS)
-
+    
+    for h in trades_stream_data:
+        if len(h) == 0:
+            rate_direction_predictor(trades_stream_data, trade_units_available, structured_price_data, INSTRUMENTS)
+            print('First trade created')
+            time.sleep(5)
+        else:
+            following_trades_creator(trades_stream_data, compare_heartbeat, trade_units_available, structured_price_data, INSTRUMENTS)
+            print('I am active. The fund is working. Relax!')
+            time.sleep(5)
+            pass
     # TIME NOW?? datetime.strftime(datetime.utcnow(), '%Y-%m-%dT%H:%M:%SZ')
+
+# ******************************************************************
+# 
+# Unusual behaviour - sometimes the function doesn't set the right Take Profit value
+# Moreover it overwrites the existing value with the wrong one.
+# 
+# ******************************************************************
+# ERROR-ERROR-ERROR
+#     Traceback (most recent call last):
+#   File "c:\projects\Diplomum\env\lib\site-packages\urllib3\response.py", line 302, in _error_catcher
+#     yield
+#   File "c:\projects\Diplomum\env\lib\site-packages\urllib3\response.py", line 594, in read_chunked
+#     self._update_chunk_length()
+#   File "c:\projects\Diplomum\env\lib\site-packages\urllib3\response.py", line 536, in _update_chunk_length
+#     line = self._fp.fp.readline()
+#   File "C:\Python34\lib\socket.py", line 378, in readinto
+#     return self._sock.recv_into(b)
+#   File "C:\Python34\lib\ssl.py", line 748, in recv_into
+#     return self.read(nbytes, buffer)
+#   File "C:\Python34\lib\ssl.py", line 620, in read
+#     v = self._sslobj.read(len, buffer)
+# ConnectionResetError: [WinError 10054] Удаленный хост принудительно разорвал существующее подключение
+
+# During handling of the above exception, another exception occurred:
+
+# Traceback (most recent call last):
+#   File "c:\projects\Diplomum\env\lib\site-packages\requests\models.py", line 739, in generate
+#     for chunk in self.raw.stream(chunk_size, decode_content=True):
+#   File "c:\projects\Diplomum\env\lib\site-packages\urllib3\response.py", line 432, in stream
+#     for line in self.read_chunked(amt, decode_content=decode_content):
+#   File "c:\projects\Diplomum\env\lib\site-packages\urllib3\response.py", line 622, in read_chunked
+#     self._original_response.close()
+#   File "C:\Python34\lib\contextlib.py", line 77, in __exit__
+#     self.gen.throw(type, value, traceback)
+#   File "c:\projects\Diplomum\env\lib\site-packages\urllib3\response.py", line 320, in _error_catcher
+#     raise ProtocolError('Connection broken: %r' % e, e)
+# urllib3.exceptions.ProtocolError: ("Connection broken: ConnectionResetError(10054, 'Удаленный хост принудительно разорвал существующее подключение', None, 10054, None)", ConnectionResetError(10054, 'Удаленный хост принудительно разорвал существующее подключение', None, 10054, None))
+
+# During handling of the above exception, another exception occurred:
+
+# Traceback (most recent call last):
+#   File "assembly_spot.py", line 300, in <module>
+#     following_trades_creator(trades_stream_data, compare_heartbeat, trade_units_available, structured_price_data, INSTRUMENTS)
+#   File "assembly_spot.py", line 220, in following_trades_creator
+#     for price_lists in structured_price_data:
+#   File "assembly_spot.py", line 73, in read_stream_data_generator
+#     for rate in stream_generator:
+#   File "assembly_spot.py", line 61, in stream_rates_generator
+#     for line in r.iter_lines():
+#   File "c:\projects\Diplomum\env\lib\site-packages\requests\models.py", line 783, in iter_lines
+#     for chunk in self.iter_content(chunk_size=chunk_size, decode_unicode=decode_unicode):
+#   File "c:\projects\Diplomum\env\lib\site-packages\requests\models.py", line 742, in generate
+#     raise ChunkedEncodingError(e)
+# requests.exceptions.ChunkedEncodingError: ("Connection broken: ConnectionResetError(10054, 'Удаленный хост принудительно разорвал существующее подключение', None, 10054, None)", ConnectionResetError(10054, 'Удаленный хост принудительно разорвал существующее подключение', None, 10054, None))
